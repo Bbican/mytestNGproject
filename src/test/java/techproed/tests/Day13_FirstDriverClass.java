29b4d6d17d1214b619f3e6d87806bc0c5e0e8212
Bbican <95518981+Bbican@users.noreply.github.com>
2022-01-18 22:08:41 -0500

    selenium

---
+++ src/test/java/techproed/tests/Day13_FirstDriverClass.java
@@ -0,0 +1,23 @@
+package techproed.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import techproed.utilities.ConfigurationReader;
+import techproed.utilities.Driver;
+
+public class Day13_FirstDriverClass {
+    @Test
+    public void firstDriverTest(){
+        // driver ===> Driver.getDriver()
+        // Driver.getDriver().get("https://www.amazon.com");
+        // Driver.getDriver().navigate().to("https://www.google.com");
+
+        // We are using configuration file to get the url
+        // We do it because of reusability and making the code more dynamic
+        Driver.getDriver().get(ConfigurationReader.getProperty("amazon_url"));
+        Driver.getDriver().navigate().to(ConfigurationReader.getProperty("google_url"));
+
+        System.out.println("Google Title :" + Driver.getDriver().getTitle());
+        Driver.closeDriver();
+    }
+}
