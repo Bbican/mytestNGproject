e8ea2aac3ea0af113e2717c994c41f26531f53ef
Bbican <95518981+Bbican@users.noreply.github.com>
2022-01-23 15:38:22 -0500

    selenium

--- src/test/java/techproed/pages/LoginPage.java
+++ src/test/java/techproed/pages/LoginPage.java
@@ -3,49 +3,43 @@ package techproed.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.w3c.dom.html.HTMLInputElement;
 import techproed.utilities.ConfigurationReader;
 import techproed.utilities.Driver;
-
 public class LoginPage {
 
+    //    Find the page object in this class
+//    creating the constructor to initialize the page element
+//    PageFactory.initElements is used to initialize this page objects
 
-    // Find the page object in this class
-    // creating the constructor to initialize the page element
-    //  PageFactory.initElements in used to initialize this page objectes
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
-    // employee info
-    // gino.wintheiser
-    // %B6B*q1!TH
+
+    //    employee info
+//   gino.wintheiser
+//  %B6B*q1!TH
+
     @FindBy(id="account-menu")
     public WebElement loginDropdown;
-
-    @FindBy(id = "login-item")
+    @FindBy(xpath = "(//*[text()='Sign in'])[1]")
     public WebElement signInButton;
-
-    @FindBy(id="username")
+    @FindBy(id = "username")
     public WebElement username;
-
-    @FindBy(id="password")
+    @FindBy(id = "password")
     public WebElement password;
-
     @FindBy(xpath = "//button[@type='submit']")
     public WebElement loginButton;
-
     @FindBy(xpath = "//span[.='Sign out']")
     public WebElement signOut;
 
     //    Creating a reusable login method
 //    I will call this in the test classes to login
+
     public void loginApplication(String userName, String pass){
         loginDropdown.click();
         signInButton.click();
         username.sendKeys(userName);
         password.sendKeys(pass);
         loginButton.click();
-
     }
-    }
-
+}
--- src/test/java/techproed/tests/smoketest/Day13_PositiveLogin.java
+++ src/test/java/techproed/tests/smoketest/Day13_PositiveLogin.java
@@ -6,13 +6,47 @@ import techproed.utilities.ConfigurationReader;
 import techproed.utilities.Driver;
 
 public class Day13_PositiveLogin {
+    /*
+        Testing important functionalities of the application
+       1. To check if the application up and running
+       2. To check if critical functionalities are working
+      In a smoke test:
+      -login
+      -log out
+      -create account
+      -add to card
+      -make payment
+      --------
+      There are 21 test scripts in my smoke
+      Smoke test automation script should take abaut 15-20 minute an avrage.
+      -------
+      Who chooses to include functionalities in the smoke suit.
+      -Test Leads(Senior testers who knows the application very good), chooses what goes to smoke tyest suit.
+      ----------
+      Regression test:
+      1. Testing ALL major functionalities of the application
+      - All smoke tests +
+      - Loan creation +
+      - account creation, edit, delete,....
+      - search
+      -... many more
+      - There are 600 Test scripts
+      - 8-10 hours
+      -----------------------------
+      When do you do regression testing?
+      1. Before every release.(minor(every 3 months) or major release(Every 6 months))
+      2. After each major buy fix. To make sure there is no impact on the existing functionalities
+      ------------------------------
+      Unit test < integration test(testing if components are working good together) < regression test
+
+     */
 
     LoginPage loginPage = new LoginPage();
     @Test
     public void login(){
         Driver.getDriver().get(ConfigurationReader.getProperty("gmi_login_url"));
         loginPage.loginDropdown.click();
-        loginPage.singInButton.click();
+        loginPage.signInButton.click();
         loginPage.username.sendKeys(ConfigurationReader.getProperty("employee_username"));
         loginPage.password.sendKeys(ConfigurationReader.getProperty("employee_passsword"));
         loginPage.loginButton.click();
